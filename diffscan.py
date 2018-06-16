a2e6f6a3721188ed42843400e321d2a7c3b6a532
Juraj Kosik <juraj.kosik@telekom.com>
2018-06-16 22:27:47 +0200

    v3

--- diffscan.py
+++ diffscan.py
@@ -6,17 +6,23 @@ from vault import *
 import subprocess
 import shlex
 import os
-import time
+import shutil
+import datetime
 import requests
 import json
 
 #rename last scan result to xxx.old 
-def version_files():
+def version_files(target):
     abspath = os.path.abspath("outputs")
-    for filename in os.listdir(abspath):
-        os.rename(abspath+"/"+filename, abspath+"/"+filename+".old")
+    if not os.path.exists(abspath+"/"+target):
+        os.makedirs(abspath+"/"+target)
+    for filename in os.listdir(abspath+"/"+target):
+        print("filenames in outputs/target: ", filename)
+        if filename != target+".out": os.remove(abspath+"/"+target+"/"+filename) #remove everything but target.out
+        if filename == target+".out": os.rename(abspath+"/"+target+"/"+filename, abspath+"/"+target+"/"+filename+".old") #version only target.out
 
 def scan(target):
+    print("Scanning...")
     cmd = "masscan -c configs/{0}.conf".format(target)
     args = shlex.split(cmd)
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -24,8 +30,11 @@ def scan(target):
     
 
 def compare(target):
-    old = "outputs/"+target+".out.old"
-    new = "outputs/"+target+".out"
+    print("Comparing...")
+    url = SLACK_WEBHOOK
+    old = "outputs/"+target+"/"+target+".out.old"
+    new = "outputs/"+target+"/"+target+".out"
+    print(new, old)
     if os.path.exists(old):
         print("Comparing {0} and {1}:".format(old, new))
         with open (old, "r") as myfile1:
@@ -51,23 +60,20 @@ def compare(target):
             text += "*Target - {0}: Full scan results:*\n".format(target)
             for i in newfile:
                 text += (i)
-            url = SLACK_WEBHOOK
-            data = {"channel":"#sec_va_pentest", "username":"DIFFSCAN", "text":text, "icon_emoji":":pentest:"}
+            data = {"channel":SLACK_CHANNEL, "username":"DIFFSCAN", "text":text, "icon_emoji":":pentest:"}
             headers = {'Content-type': 'application/json'}
             r = requests.post(url, data=json.dumps(data), headers=headers)
         else:
             print("Target - {0}: No new records found in the last scan.".format(target))
             
             '''print for slack'''
-            url = SLACK_WEBHOOK
             text_nonew = "*Target - {0}:* No new records found in the last scan.".format(target)
-            data = {"channel":"#sec_va_pentest", "username":"DIFFSCAN", "text":text_nonew, "icon_emoji":":pentest:"}
+            data = {"channel":SLACK_CHANNEL, "username":"DIFFSCAN", "text":text_nonew, "icon_emoji":":pentest:"}
             headers = {'Content-type': 'application/json'}
             r = requests.post(url, data=json.dumps(data), headers=headers)
 
     else:
         print("Older scan results not found. Nothing to compare.")
-
         '''print for slack'''
         text = "*Target - {0}*: Nothing to compare yet. Full scan results:\n".format(target)
         print(text)
@@ -75,40 +81,32 @@ def compare(target):
             newfile=myfile2.readlines()
         for i in newfile:
             text += (i)
-        url = SLACK_WEBHOOK
-        data = {"channel":"#sec_va_pentest", "username":"DIFFSCAN", "text":text, "icon_emoji":":pentest:"}
+        data = {"channel":SLACK_CHANNEL, "username":"DIFFSCAN", "text":text, "icon_emoji":":pentest:"}
         headers = {'Content-type': 'application/json'}
         r = requests.post(url, data=json.dumps(data), headers=headers)
 
 #cleanup - keep only latest(actual) results in /outputs. Versioned old files are not needed after diff. Moving them to /history...
-def remove_mess():
-    to_archive = [i + ".out.old" for i in targets] #list of files to archive
-    for file in os.listdir("outputs"):
-        if file in to_archive:
-            os.rename("outputs/"+file, "history/"+file+str(time.time()))
-    to_keep = [i + ".out" for i in targets] #list of files to keep in /outputs for next round
-    for file in os.listdir("outputs"):
-        if file not in to_keep:
-            os.remove("outputs/"+file)
+def archive(target):
+    shutil.copyfile("outputs/"+target+"/"+target+".out", "history/"+target+".out."+str(datetime.datetime.now().strftime('%Y%m%d-%H%M%S')))
 
 
 ### EXEC
 
-#create list of targets based on filenames in /config, e.g. ['test', 'as1234']
+#create list of potential targets
 targets = []
 for conf in os.listdir("configs"):
     name, extension = os.path.splitext(conf)
     targets.append(name)
 print("Configs available: ", targets)
 
-version_files()
-print("Dir /outputs after versioning: ",os.listdir("outputs"))
+targets_to_use = ['as1902'] #add configs to be used
+for i in targets_to_use:
+    version_files(i)
+    scan(i)
+    compare(i)
+    archive(i)
 
-scan('test')
-compare('test')
-
-remove_mess()
-print("Dir /outputs after cleanup: ",os.listdir("outputs"))
+print("/outputs dir: ",os.listdir("outputs"))
 
 
 
