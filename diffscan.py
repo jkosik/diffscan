ebcdb488c327e19c1c8e54dd8dcd0e20e2042c63
Juraj Kosik <juraj.kosik@telekom.com>
2018-06-19 09:50:48 +0200

    v4 nmap

--- diffscan.py
+++ diffscan.py
@@ -23,11 +23,19 @@ def version_files(target):
 
 def scan(target):
     print("Scanning {0}...".format(target))
-    cmd = "masscan -c configs/{0}.conf".format(target)
-    args = shlex.split(cmd)
-    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out = p.communicate()[0]
-    
+    cmd1 = "nmap -Pn -iL configs/{0}.conf --top-ports 10 -oG outputs/{0}/{0}.out --open".format(target)
+    cmd2 = "sed -i '/Ports/!d' outputs/{0}/{0}.out".format(target)
+    cmd3 = "cp outputs/{0}/{0}.out outputs/{0}/{0}.out-tmp".format(target) # cut can not easily cut in place
+    #cmd4 = "cut -d'/' -f1-5 outputs/{0}/{0}.out-tmp > outputs/{0}/{0}.out".format(target) #would not run within subprocess.check_call
+    cmd4 = "cut -d'/' -f1-5 outputs/{0}/{0}.out-tmp".format(target) # cut can not easily cut in place. stdout managed by subprocess.Popen
+    cmd5 = "rm outputs/{0}/{0}.out-tmp".format(target) # cut can not easily cut in place
+    subprocess.check_call(shlex.split(cmd1))
+    subprocess.check_call(shlex.split(cmd2))
+    subprocess.check_call(shlex.split(cmd3))
+    out_file_path = "outputs/{0}/{0}.out".format(target) #check_call not usable when stdout needs to be send to file 
+    out_file = open(out_file_path, 'w+')
+    subprocess.Popen(shlex.split(cmd4), stdout=out_file, stderr=subprocess.PIPE)
+    subprocess.check_call(shlex.split(cmd5))
 
 def compare(target):
     print("Comparing with the previous scan...")
@@ -40,9 +48,7 @@ def compare(target):
             oldfile=myfile1.readlines()
         with open (new, "r") as myfile2:
             newfile=myfile2.readlines()
-        oldfile_nocomments = [x for x in oldfile if not x.startswith('#')]
-        newfile_nocomments = [x for x in newfile if not x.startswith('#')]
-        diff = list(set(newfile_nocomments) - set(oldfile_nocomments)) #diff new and old
+        diff = list(set(newfile) - set(oldfile)) #diff new and old
         stripped_diff = [s.rstrip() for s in diff] #strip new lines
         non_empty_stripped_diff = list(filter(None, stripped_diff)) #filter out elements evaluated to False (e.g. empty elements)
 #        print(non_empty_stripped_diff)
@@ -98,7 +104,7 @@ for conf in os.listdir("configs"):
     targets.append(name)
 print("Configs available: ", targets)
 
-targets_to_use = ['as1902'] #add configs to be used
+targets_to_use = ['test'] #add configs to be used
 for i in targets_to_use:
     print("=== RUNNING DIFFSCAN AGAINST {0} ===".format(i))
     version_files(i)
